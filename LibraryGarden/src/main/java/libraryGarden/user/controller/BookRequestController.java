1ce91320e7f06f8c4a628c6bb5657f6f68c87070
biso15 <biso150@gmail.com>
2025-03-21 23:55:50 +0900

    AdminBookRequestController 이름 변경

--- LibraryGarden/src/main/java/libraryGarden/user/controller/BookRequestController.java
+++ LibraryGarden/src/main/java/libraryGarden/user/controller/BookRequestController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminBookRequestController {
+public class BookRequestController {
 
 //	@GetMapping("/user/userPrivacyPolicy.do")
 //    public String userPrivacyPolicy() {
