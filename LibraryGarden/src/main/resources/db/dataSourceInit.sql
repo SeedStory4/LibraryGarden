7edda285318897605e82e4641fbfcf459298f9d8
biso15 <biso150@gmail.com>
2025-03-23 22:26:00 +0900

    data init 쿼리 수정(CATEGORY,LIBRARYBOOKS,LOAN,OVERDUE 테이블 생성 및 샘플 데이터 생성)

--- LibraryGarden/src/main/resources/db/dataSourceInit.sql
+++ LibraryGarden/src/main/resources/db/dataSourceInit.sql
@@ -4,6 +4,18 @@ DROP TABLE IF EXISTS APPROVAL;
 -- REQUEST 테이블 삭제
 DROP TABLE IF EXISTS REQUEST;
 
+-- OVERDUE 테이블 삭제
+DROP TABLE IF EXISTS OVERDUE;
+
+-- LOAN 테이블 삭제
+DROP TABLE IF EXISTS LOAN;
+
+-- LIBRARYBOOKS 테이블 삭제
+DROP TABLE IF EXISTS LIBRARYBOOKS;
+
+-- CATEGORY 테이블 삭제
+DROP TABLE IF EXISTS CATEGORY;
+
 -- BOOKS 테이블 삭제
 DROP TABLE IF EXISTS BOOKS;
 
@@ -38,11 +50,78 @@ CREATE TABLE BOOKS (
     totalPages INT,
     isbn VARCHAR(20) NOT NULL,
     info VARCHAR(30),
+    category VARCHAR(30),
     introduction TEXT NOT NULL,
     price INT NOT NULL,
     regDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modify DATETIME,
     delyn CHAR(1) NOT NULL DEFAULT 'N'
+);
+
+-- CATEGORY 테이블 생성
+CREATE TABLE CATEGORY (
+    cidx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    parentCode VARCHAR(3) NOT NULL,
+    childCode VARCHAR(3) NOT NULL,
+    level INT NOT NULL,
+    regdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modify DATETIME,
+    delyn CHAR(1) NOT NULL DEFAULT 'N'
+);
+
+-- LIBRARYBOOKS 테이블 생성
+CREATE TABLE LIBRARYBOOKS (
+    lbidx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bidx INT NOT NULL,
+    cidx INT NOT NULL,
+    code VARCHAR(30) NOT NULL,
+    callName VARCHAR(30) NOT NULL,
+    location VARCHAR(50) NOT NULL DEFAULT '일반열람실',
+    loanDate DATE,
+    dueDate DATE,
+    returnDate DATE,
+    status VARCHAR(50) NOT NULL DEFAULT '대출가능',
+    extended CHAR(1) NOT NULL DEFAULT 'N',
+    regdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modify DATETIME,
+    CONSTRAINT fk_librarybooks_books FOREIGN KEY (bidx) REFERENCES BOOKS(bidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_librarybooks_category FOREIGN KEY (cidx) REFERENCES CATEGORY(cidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- LOAN 테이블 생성
+CREATE TABLE LOAN (
+    lidx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lbidx INT NOT NULL,
+    uidx INT NOT NULL,
+    loanDate DATE NOT NULL,
+    dueDate DATE NOT NULL,
+    returnDate DATE,
+    status VARCHAR(50) NOT NULL DEFAULT '대여중',
+    extended CHAR(1) NOT NULL DEFAULT 'N',
+    regdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modify DATETIME,
+    delyn CHAR(1) NOT NULL DEFAULT 'N',
+    CONSTRAINT fk_loan_librarybooks FOREIGN KEY (lbidx) REFERENCES LIBRARYBOOKS(lbidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_loan_user FOREIGN KEY (uidx) REFERENCES USER(uidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- OVERDUE 테이블 생성
+CREATE TABLE OVERDUE (
+    oidx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lidx INT NOT NULL,
+    uidx INT NOT NULL,
+    status CHAR(1) NOT NULL DEFAULT 'Y',
+    startDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    endDate DATETIME,
+    CONSTRAINT fk_overdue_loan FOREIGN KEY (lidx) REFERENCES LOAN(lidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_overdue_user FOREIGN KEY (uidx) REFERENCES USER(uidx) 
+        ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- REQUEST 테이블 생성
@@ -87,7 +166,8 @@ CREATE TABLE APPROVAL (
 INSERT INTO USER(id, password, name, phone, email, address, userNumber, role) 
 VALUES ('director','1234','도서관장','01012345678','director@aaa.ccc','대한민국','00000000','도서관장'), 
 ('librarian','1234','사서','01012566211','librarian@aaa.ccc','대한민국','00000010','사서'), 
-('user','1234','홍길동','0101112223','user1@aaa.ccc','대한민국','12344444','일반회원');
+('user1','1234','홍길동','0101112223','user1@aaa.ccc','대한민국','12344444','일반회원'),
+('user2','1234','이순신','0101112224','user2@aaa.ccc','대한민국','12344445','일반회원');
 
 -- BOOKS 샘플 데이터 생성
 INSERT INTO BOOKS(coverImg, title, subtitle, author, publisher, publishedYear, totalPages, isbn, info, introduction, price, regdate, modify, delyn) 
@@ -179,6 +259,146 @@ VALUES('https://image.aladin.co.kr/product/4086/97/cover500/8936434128_2.jpg', '
 <br><br>
 대산세계문학총서의 62번째 권으로 출간된 이 시선집에는 <비스와바 쉼보르스카 자선(自選) 시집 Wiersze wybrane>(2000)과 2002년에 출간된 <순간 Chwila>, <콜론Dwukropek>(2005)에 수록된 작품 중에서 옮긴이가 뽑은 170편의 시가 수록돼 있다. <비스와바 쉼보르스카 자선 시집>은 시인의 첫 시집 <우리가 살아가는 이유>(1952)에서부터 아홉번째 시집인 <끝과 시작>(1993)에 이르기까지 총 9권의 시집과 기타 미공개 작품들 가운데서 시인이 직접 선별한 184편의 시가 수록된 책이다.', 22000, '2025-03-22', NULL, 'N');
 
+-- CATEGORY 샘플 데이터 생성
+INSERT INTO CATEGORY (name, parentCode, childCode, level) VALUES
+('총류', '000', '000', 1),
+('도서학, 서지학', '000', '010', 2),
+('문헌정보학', '000', '020', 2),
+('백과사전', '000', '030', 2),
+('강연집, 수필집, 연설문집', '000', '040', 2),
+('일반 연속간행물', '000', '050', 2),
+('일반 학회, 단체, 협회, 기관', '000', '060', 2),
+('신문, 언론, 저널리즘', '000', '070', 2),
+('일반 전집, 총서', '000', '080', 2),
+('향토자료', '000', '090', 2),
+('철학', '100', '100', 1),
+('형이상학', '100', '110', 2),
+('인식론, 인과론, 인간학', '100', '120', 2),
+('철학의 체계', '100', '130', 2),
+('경학', '100', '140', 2),
+('아시아철학, 사상', '100', '150', 2),
+('서양철학', '100', '160', 2),
+('논리학', '100', '170', 2),
+('심리학', '100', '180', 2),
+('윤리학, 도덕철학', '100', '190', 2),
+('종교', '200', '200', 1),
+('비교종교', '200', '210', 2),
+('불교', '200', '220', 2),
+('기독교', '200', '230', 2),
+('도교', '200', '240', 2),
+('천도교', '200', '250', 2),
+('신도', '200', '260', 2),
+('파라문교, 인도교', '200', '270', 2),
+('이슬람교(회교)', '200', '280', 2),
+('기타 제종교', '200', '290', 2),
+('사회과학', '300', '300', 1),
+('통계학', '300', '310', 2),
+('경제학', '300', '320', 2),
+('사회학, 사회문제', '300', '330', 2),
+('정치학', '300', '340', 2),
+('행정학', '300', '350', 2),
+('법학', '300', '360', 2),
+('교육학', '300', '370', 2),
+('풍속, 민속학', '300', '380', 2),
+('국방, 군사학', '300', '390', 2),
+('자연과학', '400', '400', 1),
+('수학', '400', '410', 2),
+('물리학', '400', '420', 2),
+('화학', '400', '430', 2),
+('천문학', '400', '440', 2),
+('지학', '400', '450', 2),
+('광물학', '400', '460', 2),
+('생명과학', '400', '470', 2),
+('식물학', '400', '480', 2),
+('동물학', '400', '490', 2),
+('기술과학', '500', '500', 1),
+('의학', '500', '510', 2),
+('농업, 농학', '500', '520', 2),
+('공학, 공학일반', '500', '530', 2),
+('건축공학', '500', '540', 2),
+('기계공학', '500', '550', 2),
+('전기공학, 전자공학', '500', '560', 2),
+('화학공학', '500', '570', 2),
+('제조업', '500', '580', 2),
+('가정학 및 가정생활', '500', '590', 2),
+('예술', '600', '600', 1),
+('건축술', '600', '610', 2),
+('조각', '600', '620', 2),
+('공예, 장식미술', '600', '630', 2),
+('서예', '600', '640', 2),
+('회화, 도화', '600', '650', 2),
+('사진술', '600', '660', 2),
+('음악', '600', '670', 2),
+('연극', '600', '680', 2),
+('오락, 운동', '600', '690', 2),
+('언어', '700', '700', 1),
+('한국어', '700', '710', 2),
+('중국어', '700', '720', 2),
+('일본어 ', '700', '730', 2),
+('영어', '700', '740', 2),
+('독일어', '700', '750', 2),
+('프랑스어', '700', '760', 2),
+('스페인어', '700', '770', 2),
+('이탈리아어', '700', '780', 2),
+('기타 제어', '700', '790', 2),
+('문학', '800', '800', 1),
+('한국문학', '800', '810', 2),
+('중국문학', '800', '820', 2),
+('일본문학', '800', '830', 2),
+('영미문학', '800', '840', 2),
+('독일문학', '800', '850', 2),
+('프랑스문학', '800', '860', 2),
+('스페인문학', '800', '870', 2),
+('이탈리아문학', '800', '880', 2),
+('기타 제문학', '800', '890', 2),
+('역사', '900', '900', 1),
+('아시아', '900', '910', 2),
+('유럽', '900', '920', 2),
+('아프리카', '900', '930', 2),
+('북아메리카', '900', '940', 2),
+('남아메리카', '900', '950', 2),
+('오세아니아', '900', '960', 2),
+('양극지방', '900', '970', 2),
+('지리', '900', '980', 2),
+('전기', '900', '990', 2);
+
+-- LIBRARYBOOKS 샘플 데이터 생성
+INSERT INTO LIBRARYBOOKS (bidx, cidx, code, callName, location, loanDate, dueDate, returnDate, status, regdate, modify) 
+VALUES
+(1, 81, 'SS000001', '800.161.01', '일반열람실', '2025-03-23', '2025-03-30', NULL, '대출중', '2025-03-23 10:00:00', NULL),
+(2, 82, 'SS000002', '810.162.01', '일반열람실', '2025-03-21', '2025-03-28', '2025-03-28', '대출가능', '2025-03-21 11:00:00', '2025-03-28 12:00:00'),
+(3, 83, 'SS000003', '820.163.01', '일반열람실', '2025-03-18', '2025-03-25', '2025-03-25', '대출가능', '2025-03-18 09:30:00', '2025-03-25 15:00:00'),
+(4, 14, 'SS000004', '130.101.01', '일반열람실', '2025-03-22', '2025-03-29', NULL, '대출중', '2025-03-22 13:00:00', NULL), -- 대여중
+(5, 15, 'SS000005', '140.102.01', '일반열람실', '2025-03-20', '2025-03-27', '2025-03-27', '대출가능', '2025-03-20 11:30:00', '2025-03-27 12:30:00'),
+(6, 36, 'SS000006', '350.201.01', '일반열람실', '2025-03-23', '2025-03-30', NULL, '대출중', '2025-03-23 09:00:00', NULL), -- 대여중
+(7, 37, 'SS000007', '360.202.01', '일반열람실', '2025-03-15', '2025-03-22', '2025-03-22', '대출가능', '2025-03-15 13:00:00', '2025-03-22 10:30:00'),
+(8, 58, 'SS000008', '700.301.01', '일반열람실', '2025-03-20', '2025-03-27', '2025-03-27', '대출가능', '2025-03-20 14:00:00', '2025-03-27 15:30:00'),
+(9, 69, 'SS000009', '680.401.01', '일반열람실', '2025-03-16', '2025-03-23', '2025-03-23', '대출가능', '2025-03-16 10:30:00', '2025-03-23 11:00:00'),
+(10, 80, 'SS000010', '790.501.01', '일반열람실', '2025-03-14', '2025-03-21', '2025-03-23', '대출가능', '2025-03-14 08:30:00', '2025-03-23 17:00:00'),
+(11, 1, 'SS000011', '000.111.01', '일반열람실', NULL, NULL, NULL, '대출가능', '2025-03-14 15:30:00', NULL),
+(12, 2, 'SS000012', '010.112.01', '일반열람실', NULL, NULL, NULL, '대출가능', '2025-03-12 12:30:00', NULL),
+(13, 3, 'SS000013', '020.113.01', '일반열람실', NULL, NULL, NULL, '대출가능', '2025-03-15 16:30:00', NULL),
+(14, 4, 'SS000014', '030.114.01', '일반열람실', NULL, NULL, NULL, '대출가능', '2025-03-11 10:30:00', NULL),
+(15, 5, 'SS000015', '040.115.01', '일반열람실', NULL, NULL, NULL, '대출가능', '2025-03-11 08:30:00', NULL);
+
+-- LOAN 샘플 데이터 생성
+INSERT INTO LOAN (lbidx, uidx, loanDate, dueDate, returnDate, status, regdate, modify) 
+VALUES 
+(1, 3, '2025-03-23', '2025-03-30', NULL, '대여중', '2025-03-23 10:00:00', NULL), -- 대여중
+(2, 3, '2025-03-21', '2025-03-28', '2025-03-28', '반납완료', '2025-03-21 11:00:00', '2025-03-28 12:00:00'), -- 반납완료
+(3, 3, '2025-03-18', '2025-03-25', '2025-03-25', '반납완료', '2025-03-18 09:30:00', '2025-03-25 15:00:00'), -- 반납완료
+(4, 3, '2025-03-22', '2025-03-29', NULL, '대여중', '2025-03-22 13:00:00', NULL), -- 대여중
+(5, 3, '2025-03-20', '2025-03-27', '2025-03-27', '반납완료', '2025-03-20 11:30:00', '2025-03-27 12:30:00'), -- 반납완료
+(6, 4, '2025-03-23', '2025-03-30', NULL, '대여중', '2025-03-23 09:00:00', NULL), -- 대여중
+(7, 4, '2025-03-15', '2025-03-22', '2025-03-22', '반납완료', '2025-03-15 13:00:00', '2025-03-22 10:30:00'), -- 반납완료
+(8, 4, '2025-03-20', '2025-03-27', '2025-03-27', '반납완료', '2025-03-20 14:00:00', '2025-03-27 15:30:00'), -- 반납완료
+(9, 4, '2025-03-16', '2025-03-23', '2025-03-23', '반납완료', '2025-03-16 10:30:00', '2025-03-23 11:00:00'), -- 반납완료
+(10, 4, '2025-03-14', '2025-03-21', '2025-03-23', '연체반납', '2025-03-14 08:30:00', '2025-03-23 17:00:00'); -- 연체반납
+
+-- OVERDUE 샘플 데이터 생성
+INSERT INTO OVERDUE (lidx, uidx, status, startDate, endDate)
+VALUES (10, 4, 'Y', '2025-03-23', '2025-03-27');
+
 -- REQUEST 샘플 데이터 생성
 INSERT INTO REQUEST(uidx,bidx) 
 VALUES (1,1), 
@@ -197,7 +417,7 @@ VALUES (1,1),
 (2,14), 
 (1,15);
 
--- USER 샘플 데이터 생성
+-- APPROVAL 샘플 데이터 생성
 INSERT INTO APPROVAL(uidx,rqidx,bidx,status,rejectionReason) 
 VALUES (2,1,1,'반려','예산부족'), 
 (2,2,2,'승인',''), 
