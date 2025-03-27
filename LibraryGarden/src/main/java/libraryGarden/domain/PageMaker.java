d4a5b1efeaf26f95b50359b5a4b482616e13c3fa
biso15 <biso150@gmail.com>
2025-03-28 00:56:33 +0900

    결재관리 목록 및 페이징 관련 주석 추가

--- LibraryGarden/src/main/java/libraryGarden/domain/PageMaker.java
+++ LibraryGarden/src/main/java/libraryGarden/domain/PageMaker.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class PageMaker {
 
-	private int displayPageNum = 10;
-	private int startPage;
-	private int endPage;
-	private int totalCount;
+	private int displayPageNum = 10;  // 화면에 나타낼 페이지 갯수
+	private int startPage;  // 화면에 나타날 첫번째 페이지
+	private int endPage;  // 화면에 나타날 마지막 페이지
+	private int totalCount;  // 총 게시물 수
 	
-	private boolean prev;
-	private boolean next;
+	private boolean prev;  // 이전버튼
+	private boolean next;  // 다음 버튼
 	
 	private SearchCriteria scri;	
 
@@ -45,8 +45,7 @@ public class PageMaker {
 
 	public void setTotalCount(int totalCount) {
 		this.totalCount = totalCount;
-		calcData();
-		
+		calcData();		
 	}
 
 	public boolean isPrev() {
@@ -73,21 +72,40 @@ public class PageMaker {
 		this.scri = scri;
 	}
 	
+	// 화면에 나타낼 페이지 번호 계산
 	private void calcData() {
 		
-		endPage = (int)(Math.ceil(scri.getPage() / (double)displayPageNum) * displayPageNum);
+		// 1. 기본적으로 1에서부터 10까지 나타나게 설정한다 (페이지네비게이션에서)
+		endPage = (int)(Math.ceil(scri.getPage() / (double)displayPageNum) * displayPageNum);  // 모두 올림처리 하는 메소드 ceil()
+		// Math.ceil(scri.getPage() / (double)displayPageNum -> 현재 페이지를 한번에 보여주는 페이지 수로 나눠서 몇번째 페이지 네비게이션인지 확인한다.
+		// Math.ceil(scri.getPage() / (double)displayPageNum) * displayPageNum -> 페이지 네비게이션에 한번에 보여주는 페이지 수를 곱해서 마지막 페이지를 구한다.
+		// (int)(Math.ceil(scri.getPage() / (double)displayPageNum) * displayPageNum) -> int형으로 형변환
 		
+		// 2. endPage가 설정되었으면 시작페이지도 설정
 		startPage = (endPage - displayPageNum) + 1;
+		// endPage - displayPageNum -> 마지막 페이지에서 한번에 보여주는 페이지 수를 빼서 이전 페이지 네비게이션의 마지막 페이지를 확인한다.
+		// (endPage - displayPageNum) + 1 -> 이전 페이지 네비게이션의 마지막 페이지에 1을 더해서 현재 페이지 네비게이션의 처음 페이지를 구한다.
 		
+		// 3. 실제 게시물수에 따라서 endPage를 구한다
 		int tempEndPage = (int)(Math.ceil(totalCount / (double)scri.getPerPageNum()));
-		
+		// totalCount / (double)scri.getPerPageNum() -> 총 게시물수를 한 페이지에 보이는 게시물 수로 나눠서 전체 페이지수를 구한다.
+		// Math.ceil(totalCount / (double)scri.getPerPageNum()) -> 남는 게시물 갯수(나머지)가 있을수 있으니 소수점 올림 한다.
+		// (int)(Math.ceil(totalCount / (double)scri.getPerPageNum()))) -> int형으로 형변환
+
+		// 4. 설정한 endPage와 실제 endPage를 비교해서 최종 endPage를 구한다
 		if (endPage > tempEndPage) {
 			endPage = tempEndPage;
 		}
+		// endPage보다 실제endPage(tempEndPage)가 작으면 endPage를 실제endPage(tempEndPage)로 변경한다.
 		
-		prev = (startPage == 1? false : true);
+		// 5. 이전, 다음 버튼 만들기
+		prev = (startPage == 1? false : true);  // 삼항연산자 사용
+		// 시작페이지가 1이면 이전버튼을 없앤다.
 		
 		next = (endPage * scri.getPerPageNum() >= totalCount ? false : true);
+		// 마지막 페이지에 한 페이지에 보이는 게시물 수를 곱한 값이 전체 게시물수보다 크거나 같으면 현재 페이지가 게시판의 마지막 페이지이므로
+		// 다음버튼을 없앤다.
+		
 	}
 	
 }
