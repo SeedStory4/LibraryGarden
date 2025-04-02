222f19d62c73abe2851c3cfbb2efcb8929cc5189
biso15 <biso150@gmail.com>
2025-04-02 23:51:12 +0900

    RequestDto, RequestVo 추가 및 font css 수정

---
+++ LibraryGarden/src/main/java/libraryGarden/domain/RequestDto.java
@@ -0,0 +1,56 @@
+package libraryGarden.domain;
+
+public class RequestDto {
+
+	private int rqidx;
+	private String coverImg;
+	private String title;
+	private String author;
+	private String publisher;
+	private String name;
+	private String userNumber;
+
+	public int getRqidx() {
+		return rqidx;
+	}
+	public void setRqidx(int rqidx) {
+		this.rqidx = rqidx;
+	}
+	public String getCoverImg() {
+		return coverImg;
+	}
+	public void setCoverImg(String coverImg) {
+		this.coverImg = coverImg;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserNumber() {
+		return userNumber;
+	}
+	public void setUserNumber(String userNumber) {
+		this.userNumber = userNumber;
+	}
+	
+}
