6f49209bdbb5729c80cbe21566febdef0a35ce54
chan01116 <ktiun9630@gmail.com>
2025-04-09 01:49:59 +0900

    비밀번호 찾기_임시비빌번호 메일로 전송

--- LibraryGarden/src/main/resources/db/dataSourceInit.sql
+++ LibraryGarden/src/main/resources/db/dataSourceInit.sql
@@ -190,7 +190,7 @@ CREATE TABLE APPROVAL (
 INSERT INTO USER(id, password, name, phone, email, address, userNumber, role) 
 VALUES ('director','$2a$10$GlSGv1jjhBrWSk9ebqOa0uTeVUJ/RWqTLwkf1mA3pMN7ySNySkNQS','도서관장','01012345678','director@aaa.ccc','대한민국','00000000','도서관장'), 
  ('librarian','$2a$10$GlSGv1jjhBrWSk9ebqOa0uTeVUJ/RWqTLwkf1mA3pMN7ySNySkNQS','사서','01012566211','librarian@aaa.ccc','대한민국','00000010','사서'), 
- ('user1','$2a$10$GlSGv1jjhBrWSk9ebqOa0uTeVUJ/RWqTLwkf1mA3pMN7ySNySkNQS','홍길동','0101112223','user1@aaa.ccc','대한민국','12344444','일반회원'),
+ ('user1','$2a$10$GlSGv1jjhBrWSk9ebqOa0uTeVUJ/RWqTLwkf1mA3pMN7ySNySkNQS','홍길동','0101112223','ktiun9630@naver.com','대한민국','12344444','일반회원'),
  ('user2','$2a$10$GlSGv1jjhBrWSk9ebqOa0uTeVUJ/RWqTLwkf1mA3pMN7ySNySkNQS','이순신','0101112224','user2@aaa.ccc','대한민국','12344445','일반회원');
 
 -- BOOKS 샘플 데이터 생성
