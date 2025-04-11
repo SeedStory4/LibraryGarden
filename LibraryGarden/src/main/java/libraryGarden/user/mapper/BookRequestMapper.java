13014074f852084dfc43e172e2e1b73f775015ee
kimsiyeonA <kakasska1212@gmail.com>
2025-04-11 14:50:46 +0900

    mypage 도서신청관리 완료

--- LibraryGarden/src/main/java/libraryGarden/user/mapper/BookRequestMapper.java
+++ LibraryGarden/src/main/java/libraryGarden/user/mapper/BookRequestMapper.java
@@ -1,12 +1,17 @@
 package libraryGarden.user.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Param;
 
 /** [설명]BookRequestMapper 인터페이스:  사용자 희망 도서 관련 매서드
  * 
  * [주요기능] 
  * - 희망 도서 신청 (등록) 메서드
- * 
+ * - 사용자 희망 도서 신청을 조회하는 메서드
+ * - 사용자 희망 도서 신청한 수를 조회하는 메서드
+ * - 사용자 희망 도서 신청을 삭제하는 메서드
  * 
  * @author SiYeon
  *
@@ -16,5 +21,12 @@ public interface BookRequestMapper {
 	// 희망 도서 신청 (등록) 메서드
 	public int insertRequest(@Param("uidx") int uidx,@Param("bidx") int bidx);
 
-
+	// 사용자 희망 도서 신청을 조회하는 메서드
+	public List<Map<String, Object>> getUserRequestInfo(int uidx);
+	
+	// 사용자 희망 도서 신청한 수를 조회하는 메서드
+	public int getUserRequestInfoTotalCount(int uidx);
+	
+	// 사용자 희망 도서 신청을 삭제하는 메서드
+	public int deleteRequest(@Param("uidx") int uidx,@Param("bidx") int bidx,@Param("rqidx") int rqidx);
 }
