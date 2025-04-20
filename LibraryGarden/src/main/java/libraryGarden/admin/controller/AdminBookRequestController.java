7077de19ca77ed847e4e1e959ee41398e6e315f2
biso15 <biso150@gmail.com>
2025-04-21 01:37:23 +0900

    adminBookRequest 관련 controller, service, serviceImpl, mapper 파일 병합

--- LibraryGarden/src/main/java/libraryGarden/admin/controller/AdminBookRequestController.java
+++ LibraryGarden/src/main/java/libraryGarden/admin/controller/AdminBookRequestController.java
@@ -2,11 +2,16 @@ package libraryGarden.admin.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,19 +21,101 @@ import libraryGarden.admin.service.AdminBookRequestService;
 import libraryGarden.cmm.util.UrlEncoder;
 import libraryGarden.domain.PageMaker;
 import libraryGarden.domain.RequestDto;
+import libraryGarden.domain.RequestVo;
 import libraryGarden.domain.SearchCriteria;
 
 @Controller
 @RequestMapping("/admin/bookRequest")
 public class AdminBookRequestController {
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(AdminBookRequestController.class);
 
+	// AdminBookRequestService 주입
 	@Autowired(required=false)
 	private AdminBookRequestService bookRequestService;
 	
+	// PageMaker 주입 (페이징 기능)
 	@Autowired(required=false)
 	private PageMaker pm;
+	
+
+	/**
+	 * [설명] 관리자의 희망 도서 관리 페이지
+	 * 
+	 * [주요기능]
+	 * - 관리자 희망 도서 목록 페이지 이동(이하 희망 도서 목록 페이지 이동)
+	 * - 관리자 희망 도서 상세 페이지 이동(이하 희망 도서 상세 페이지 이동)
+	 * - 희망 도서 상세 페이지에서 상태 및 반려 사유 수정
+	 *  
+	 * @author SiYeon
+	 * @write 2024.03.23
+	 */
+	
+	// 희망 도서 목록 페이지 이동
+	@GetMapping("/bookRequestList.do")
+	public String bookRequestList( SearchCriteria scri, RequestVo rd, Model model) {
+		 logger.debug("AdminBookRequestService bookRequestList 들어옴");
+		 // 사용자가 입력한 검색조건과 검색어 저장
+		 pm.setScri(scri);
+		 
+		 // 현재 탭의 신청 상태 저장
+		 String filter = rd.getStatus();
+		 
+		 // 페이징을 위한 전체 데이터 갯수 
+		 int cnt = bookRequestService.getBookRequestTotalCount(scri, filter);
+		 pm.setTotalCount(cnt);
+		 
+		 // 목록에서 보여줄 데이터 
+		 ArrayList<RequestDto> rlist = bookRequestService.getBookRequestSelectAll(scri, filter);
+		 
+		 // "\" 등 검색시 오류 발생하지 않도록 검색어 encoding
+		 UrlEncoder encoder = new UrlEncoder();		 
+		 scri.setKeyword(encoder.encoding(scri.getKeyword()));
+		 
+		 model.addAttribute("rlist", rlist);
+		 model.addAttribute("pm", pm);
+		 model.addAttribute("filter", filter);
+		 
+		return "admin/bookRequest/bookRequestList";
+	}
+	
+	// 희망 도서 상세 페이지 이동
+	@GetMapping("/{rqidx}/bookRequestDetail.do")
+	public String bookRequestDetail(@PathVariable("rqidx") int rqidx, Model model) {
+		
+		Map<String, Object> rq = bookRequestService.getBookRequestSelectone(rqidx);
+		
+		model.addAttribute("rq", rq);
+		
+		return "admin/bookRequest/bookRequestDetail";
+	}
+
+	// 희망 도서 상세 페이지에서 상태 및 반려 사유 수정
+	@PostMapping("/bookRequestModify.do")
+	public String bookRequestModify(@RequestParam("status") String status, @RequestParam("rqidx") int rqidx,
+		    @RequestParam(value = "rejectionReason", required = false) String rejectionReason,Model model) {
+		System.out.println("AdminBookRequestService bookRequestModify 들어옴");
+		
+		int value = bookRequestService.modifyBookRequest(rqidx, status, rejectionReason);
+		System.out.println("AdminBookRequestService bookRequestModify value"+value);
+		
+		if(value == 0) {
+			model.addAttribute("msg", "등록을 실패했습니다. 잠시 후 다시 시도해주세요.");
+			return "redirect:/admin/bookRequest/"+rqidx+"/bookRequestDetail.do";
+
+		}
+		model.addAttribute("msg", "등록 성공했습니다");
+		return "redirect:/admin/bookRequest/"+rqidx+"/bookRequestDetail.do";
+	}
+	
+	
+	/**
+	 * [주요기능]
+	 * - 희망 도서 목록 페이지 이동(팝업)
+	 *  
+	 * @author JiHye
+	 * @write 2024.03.23
+	 */
 	
 	// 희망 도서 목록 페이지 이동(팝업)
 	@ResponseBody
