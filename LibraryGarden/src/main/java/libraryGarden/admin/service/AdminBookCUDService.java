83ccf74cd4dca731a0b93a7785d3705ca7c2792e
kimsiyeonA <kakasska1212@gmail.com>
2025-04-26 20:50:24 +0900

    siyeon 파트 admin 주석 정리

--- LibraryGarden/src/main/java/libraryGarden/admin/service/AdminBookCUDService.java
+++ LibraryGarden/src/main/java/libraryGarden/admin/service/AdminBookCUDService.java
@@ -6,21 +6,20 @@ import libraryGarden.domain.LibraryBooksVo;
 /** [설명]AdminBookCUDService 인터페이스: 관리자 도서 CUD를 위한 서비스
  * 
  * [주요기능] 
- *  - 관리자 도서관 도서 등록 메서드
- *  - 관리자 도서관 도서 삭제 메서드
- *  - 관리자 도서관 도서 수정 메서드
+ *  - 도서관 도서 등록 메서드
+ *  - 도서관 도서 삭제 메서드
+ *  - 도서관 도서 수정 메서드
  *  
  * @author SiYeon
- *
  */
 public interface AdminBookCUDService {
 
-	// 관리자 도서관 도서 등록 메서드
+	// 도서관 도서 등록 메서드
 	public int insertLibraryBooksAndUpdateApproval(LibraryBooksVo lbv);
 	
-	// 관리자 도서관 도서 삭제 메서드
+	// 도서관 도서 삭제 메서드
 	public int deleteLibraryBooksAndUpdateApproval(int lbidx);
 
-    // 관리자 도서관 도서 수정 메서드
+    // 도서관 도서 수정 메서드
 	public int modifyLibraryBooksAndUpdateApproval(LibraryBooksVo lbv);
 }
